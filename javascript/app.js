262604ec59896c1b3b30f9f21bbd1680b512ebed
Lotta Melcher <lotta.melcher@code.berlin>
2020-06-03 11:53:27 +0200

    Add: Second Hint Layout index.html

--- javascript/app.js
+++ javascript/app.js
@@ -1,17 +1,19 @@
 let codemaker = new Codemaker();
 let codebreaker = new Codebreaker();
 let mastercode = codemaker.generateCode();
-
 let tries = 0;
+
 while (tries <= 12) {
   if (tries === 12) {
     alert("You've lost!");
     break;
   }
+
   let currentGuess = codebreaker.makeGuess();
   document.getElementById("currentGuess").innerHTML = codebreaker.currentGuess;
   let hintCount = codemaker.compareCode(currentGuess);
-  document.getElementById("hintCounter").innerHTML = hintCount;
+  document.getElementById("hintCounter1").innerHTML = hintCount;
+
   if (hintCount === 4) {
     alert("you've won!");
     break;
