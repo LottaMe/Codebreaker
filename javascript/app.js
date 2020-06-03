d815a52d9277520119b1849f806aae8514c74159
Lotta Melcher <lotta.melcher@code.berlin>
2020-06-03 11:28:40 +0200

    Add: Task 1 in Theory Done

--- javascript/app.js
+++ javascript/app.js
@@ -2,8 +2,15 @@ let codemaker = new Codemaker();
 let codebreaker = new Codebreaker();
 let mastercode = codemaker.generateCode();
 
-let currentGuess = codebreaker.makeGuess();
-
-document.getElementById("currentGuess").innerHTML = codebreaker.currentGuess;
-let hintCount = codemaker.compareCode(currentGuess);
-document.getElementById("hintCounter").innerHTML = hintCount;
+let tries = 0;
+while (tries <= 12) {
+  let currentGuess = codebreaker.makeGuess();
+  document.getElementById("currentGuess").innerHTML = codebreaker.currentGuess;
+  let hintCount = codemaker.compareCode(currentGuess);
+  document.getElementById("hintCounter").innerHTML = hintCount;
+  if (hintCount === 4) {
+    alert("you've won!");
+    break;
+  }
+  tries++;
+}
